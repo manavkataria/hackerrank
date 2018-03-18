bb76091325d0c8bd483bd72f00d73510e4a78c10
Manav Kataria <manavkataria.iitb@gmail.com>
2018-03-17 21:49:20 -0700

    CoinChange: DP Impl

--- coin_change_dp.py
+++ coin_change_dp.py
@@ -12,9 +12,9 @@ denominations = [1, 5, 10, 25]
 
 class CoinChange(object):
 
-    def __init__(self, denominations):
+    def __init__(self, amount, denominations):
         self.denominations = denominations
-        self.cache
+        self.cache = [inf] * amount  # NOTE: Important to Initialize Cache
 
     def find_min_path(self, residual_amount):
         if residual_amount < 0:
@@ -22,21 +22,22 @@ class CoinChange(object):
 
         # print 'Res:', residual_amount
         # import ipdb; ipdb.set_trace()
-        if residual_amount in self.denominations:
-            return 1
-        else:
-            min_len = inf
-            for d in self.denominations:
-                path_len = self.find_min_path(residual_amount - d) + 1
-                if path_len < min_len:
-                    min_len = path_len
+        if self.cache[residual_amount] == inf:
+            if residual_amount in self.denominations:
+                self.cache[residual_amount] = 1
+            else:
+                min_len = inf
+                for d in self.denominations:
+                    path_len = self.find_min_path(residual_amount - d) + 1
+                    if path_len < min_len:
+                        min_len = path_len
+                self.cache[residual_amount] = min_len
 
-            return min_len
+        return self.cache[residual_amount]
 
 def main():
-    cc = CoinChange(denominations)
-    # selected = list()
-    for i in range(76):
+    cc = CoinChange(amount+1, denominations)
+    for i in range(amount+1):
         print 'Amount: %d -> Coins: %0.0f' % (i, cc.find_min_path(i))
 
 if __name__ == '__main__':
