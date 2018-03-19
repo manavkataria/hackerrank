a00cb65b2f3f6589d99b09a7d18cb78930430bb2
Manav Kataria <manavkataria.iitb@gmail.com>
2018-03-19 01:57:38 -0700

    Iterative DP up solution

--- knights_tour.py
+++ knights_tour.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python2
-
 from __future__ import print_function
+
+
+"""
+TestCase:
+10000
+10
+9876
+8
+1234
+2
+
+Expected Output:
+4322
+"""
 
 import os
 import sys
@@ -19,7 +32,7 @@ delta = (
 
 inf = float ('inf')
 
-def recHelper(graph, cur, end):
+def knights_tour_recursive(graph, cur, end):
     rows = len(graph)
     cols = len(graph[0])
 
@@ -39,7 +52,7 @@ def recHelper(graph, cur, end):
     minpathlen = inf
     for d in delta:
         node = (cur[0] + d[0], cur[1] + d[1])
-        pathlen = recHelper(graph, node, end)
+        pathlen = knights_tour_recursive(graph, node, end)
         if pathlen != inf:
             graph[node[0]][node[1]] = (graph[node[0]][node[1]][0], pathlen)
 
@@ -53,6 +66,41 @@ def recHelper(graph, cur, end):
 
     return 1 + minpathlen
 
+def knights_tour_iterative(graph, cur, end, start=None):
+    rows = len(graph)
+    cols = len(graph[0])
+
+    graph[end[0]][end[1]] = ('e', 0)  # Initialized End Val
+    queue = [end]  # Queue Inited
+    cur = end
+
+    while queue:
+        cur = queue.pop(0)
+        # print('\nCur:', cur)
+        # print('Queue:', queue)
+        # print('Graph:')
+        # for row in graph:
+        #     print(row)
+
+        graph[cur[0]][cur[1]] = ('v', graph[cur[0]][cur[1]][1])
+
+        pathlen = inf
+        for d in delta:
+            node = (cur[0] + d[0], cur[1] + d[1])
+            pathlen = 1 + graph[cur[0]][cur[1]][1]
+
+            if pathlen != inf and (0 <= node[0] < rows and 0 <= node[1] < cols) and graph[node[0]][node[1]][0] != 'v':
+                graph[node[0]][node[1]] = ('v', pathlen)
+
+                queue.append(node)
+
+            # Term Condition
+            if node == start:
+                break
+
+    return graph[start[0]][start[1]][1]
+
+
 def find_minimum_number_of_moves(rows, cols, start_row, start_col, end_row, end_col):
     graph = [[(' ', inf) for _ in range(cols)] for _ in range(rows)]
     plen = 0
@@ -62,7 +110,8 @@ def find_minimum_number_of_moves(rows, cols, start_row, start_col, end_row, end_
     graph[cur[0]][cur[1]] = ('s', inf)
     graph[end[0]][end[1]] = ('e', inf)
 
-    val = recHelper(graph, cur, end)
+    # val = knights_tour_recursive(graph, cur, end)
+    val = knights_tour_iterative(graph, cur, end, (start_row, start_col))
     return val if val != inf else -1
 
 ## Boilerplate
